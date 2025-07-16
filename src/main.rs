a74b6377451fc35f6aff53a454318b604930865c
Rami Kader <ramy.kader.rk@gmail.com>
2025-07-16 14:03:53 +0200

    feat: support --day arg

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod input;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "aoc2024")]
+#[command(author = "rkv0id")]
+#[command(about = "Run Advent of Code 2024 solutions", long_about = None)]
+struct Args {
+    #[arg(long, value_parser = clap::value_parser!(u8).range(1..=25))]
+    day: u8,
+}
+
 fn main() {
-    let input = input::fetch_input(2024, 1);
-    println!("Day 1 Input:\n{}", input);
+    let args = Args::parse();
+    println!("Running solution for Day {}", args.day);
+
+    let input = input::fetch_input(2024, args.day);
+    println!("Fetched input for Day {}:\n{}", args.day, input);
 }
